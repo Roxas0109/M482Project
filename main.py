f09c15f7b685dde933b102ab126fba2fe3fc8a52
Aaron Rahnama <aaronrahnama13@gmail.com>
2021-11-15 14:51:06 -0800

    Update main.py
    switched from 3 lists to 1 2d list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random as rand
 
-s1 = []
-s2 = []
-s3 = []
+tower = []
 
 def find_repeat(numbers):
     seen = set()
@@ -14,27 +12,20 @@ def find_repeat(numbers):
 
 
 def rotate(index):
-    temp = s1[index]
-    s1[index] = s2[index]
-    s2[index] = s3[index]
-    s3[index] = temp
+    temp = tower[index][0]
+    tower[index][0] = tower[index][1]
+    tower[index][1] = tower[index][2]
+    tower[index][2] = temp
 
 
 
 
 def generate():
-    masterList = list(range(0,6)) *3
+    n = 31
+    masterList = list(range(0,31)) *3
     rand.shuffle(masterList)
-    #s1 = masterList[:31]
-    #s2 = masterList[31:62]
-    #s3 = masterList[62:]
-    
-    s1 = masterList[:6]
-    s2 = masterList[6:12]
-    s3 = masterList[12:]
-    print(find_repeat(s1), s1)
-    print(find_repeat(s2), s2)
-    print(find_repeat(s3), s3)
+    sublists = [masterList[i:i+n] for i in range(0, len(masterList), n)]
+    print(sublists)
 
 
 
