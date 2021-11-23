3017cf004717ee68e1ad9ccded3550e259c7651f
Alan <alanhp2@hotmail.com>
2021-11-22 19:59:45 -0800

    cleaned code

--- generators.py
+++ generators.py
@@ -1,19 +1,25 @@
 import numpy as np
 
+#puzzle 1
 def p1(n):
     return int(1+(np.floor(n * 17*np.power(np.pi, 6)) % 31))
 
+#puzzle 2
 def p2(n):
     return int(1+(np.floor(n * 17*np.power(np.e, 6)) % 31))
 
+#puzzle 3
 def p3(n):
     return int(1+(np.floor(n * 17*np.power(np.e, 8)) % 31))
 
+#puzzle 4
 def p4(n):
     return int(1+(np.floor(n * 11*np.power(np.e, 8)) % 31))
 
+#puzzle 5
 def p5(n):
     return int(1+(np.floor(n * 101*np.power(np.e, 2)) % 31))
 
+#puzzle 6
 def p6(n):
     return int(1+(np.floor(n * 101*np.power(np.e, 8)) % 31))
--- main.py
+++ main.py
@@ -2,45 +2,46 @@ import random as rand
 from dict import graph
 from generators import p1
 
-#func to generate numbers
+# func to generate numbers
+
+
 def generate():
-    #init master list
+    # init master list
     masterList = []
-    #bool for while loop
+    # bool for while loop
     done = False
-    #dictionary to keep track of repetitions
+    # dictionary to keep track of repetitions
     tracker = {}
-    #var for current number being generated
+    # var for current number being generated
     i = 0
 
-    #loop
+    # loop
     while(not done):
-        #calc number at current index starting from 1
+        # calc number at current index starting from 1
         calcNum = p1(i+1)
 
-        #if generated num isn't in tracker, add it in array and tracker
+        # if generated num isn't in tracker, add it in array and tracker
         if(calcNum not in tracker):
             tracker[calcNum] = 1
             masterList.append(calcNum)
-        #if generated num already exists
+        # if generated num already exists
         else:
-            #check if < 3 occurances, is so increment occurance and add to list
+            # check if < 3 occurances, if so increment occurance and add to list
             if(tracker[calcNum] < 3):
                 tracker[calcNum] += 1
                 masterList.append(calcNum)
-        #if length of array = 93, stop loop
+        # if length of array = 93, stop loop
         if(len(masterList) == 93):
             done = True
-        #increment
+        # increment
         i += 1
-    print(tracker)
-    print(len(tracker.keys()))
-    print(masterList)
+
+    tower = [masterList[i:i+3] for i in range(0, len(masterList), 3)]
+    return tower
 
 
 if __name__ == "__main__":
-    #call func to generate the tower
+    # call func to generate the tower
     tower = generate()
-    # print(tower)
-    # print(find_repeat2(tower))
-    # print(graph)
+    print(*tower, sep='\n')
+    print('Slices: ', len(tower))
