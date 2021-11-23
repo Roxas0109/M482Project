374604a00a6ea8190d585407815075c3c2abb97e
Alan <alanhp2@hotmail.com>
2021-11-22 19:25:55 -0800

    working on generator

--- main.py
+++ main.py
@@ -2,55 +2,66 @@ import random as rand
 from dict import graph
 from generators import p1
 
-tower = []
 
-#given a list finds the repeat numbers themselves not the index
-def find_repeat(numbers):
-    repeats = []
-    seen = set()
-    for index,num in enumerate(numbers):
-        if num in seen:
-            repeats.append(num)
-        seen.add(num)
-    return repeats
+# def rotate(index):
+#     temp = tower[index][0]
+#     tower[index][0] = tower[index][1]
+#     tower[index][1] = tower[index][2]
+#     tower[index][2] = temp
 
-#given 2d list gives the number of the repeat in that list coresponding the the given index
-def find_repeat2(tower):
-    repeats = [None]*31
-    seen = set()
-    for index,row in enumerate(tower):
-        print(index, row)
-        seen = set()
-        for num in row:
-            if num in seen:
-                repeats[index] = num
-            seen.add(num)
-            graph[num]+=[index]
-    return repeats
+# generated the tower in 31*3 array, can be switch changing the value of n
+# def generate():
+#     n = 3
+#     masterList = list(range(0,31)) *3
+#     for i in masterList:
+#         masterList[i] = p1(i+1)
+#     print(masterList)
+#     tower = [masterList[i:i+n] for i in range(0, len(masterList), n)]
+#     return tower
 
+# while list is not done iterate
+# if num is not in tracker, add
+# if number doesn't have 3 occurances, add to masterList
 
-def rotate(index):
-    temp = tower[index][0]
-    tower[index][0] = tower[index][1]
-    tower[index][1] = tower[index][2]
-    tower[index][2] = temp
-
-#generated the tower in 31*3 array, can be switch changing the value of n
+#func to generate numbers
 def generate():
-    n = 3
-    masterList = list(range(0,31)) *3
-    for i in masterList:
-        masterList[i] = p1(i+1)
-    print(masterList)
-    # rand.shuffle(masterList)
-    tower = [masterList[i:i+n] for i in range(0, len(masterList), n)]
-    return tower
-    
+    #init master list
+    masterList = []
+    #bool for while loop
+    done = False
+    #dictionary to keep track of repetitions
+    tracker = {}
+    #var for current number being generated
+    i = 0
 
+    #loop
+    while(not done):
+        #calc number at current index starting from 1
+        calcNum = p1(i+1)
+
+        #if generated num isn't in tracker, add it in array and tracker
+        if(calcNum not in tracker):
+            tracker[calcNum] = 1
+            masterList.append(calcNum)
+        #if generated num already exists
+        else:
+            #check if < 3 occurances, is so increment occurance and add to list
+            if(tracker[calcNum] < 3):
+                tracker[calcNum] += 1
+                masterList.append(calcNum)
+        #if length of array = 93, stop loop
+        if(len(masterList) == 93):
+            done = True
+        #increment
+        i += 1
+    print(tracker)
+    print(len(tracker.keys()))
+    print(masterList)
 
 
 if __name__ == "__main__":
+    #call func to generate the tower
     tower = generate()
-    print(tower)
+    # print(tower)
     # print(find_repeat2(tower))
     # print(graph)
