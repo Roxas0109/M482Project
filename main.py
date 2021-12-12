6625045085e1b723dd8fff736d487a01105cd9d8
Aaron Rahnama <aaronrahnama13@gmail.com>
2021-12-12 14:42:40 -0800

    Final version
    Thank me later lol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from generators import p1,p2,p3,p4,p5,p6
 from collections import deque
-from checker import solve1
+from checker import solve
 import datetime
 from math import comb
 from itertools import combinations
@@ -22,7 +22,7 @@ def generate():
     while(True):
 
         # calc number at current index starting from 1
-        calcNum = p3(i+1)
+        calcNum = p6(i+1)
 
         # if generated num isn't in tracker, add it in array and tracker
         if(calcNum not in tracker):
@@ -43,8 +43,8 @@ def generate():
         # increment
         i += 1
 
-    #create tower list with deques of size 3 to represent slices
-    tower = [deque(masterList[i:i+3]) for i in range(0, len(masterList), 3)]
+    #create tower list of size 3 to represent slices
+    tower = [masterList[i:i+3] for i in range(0, len(masterList), 3)]
     return tower
 
 
@@ -55,33 +55,31 @@ if __name__ == "__main__":
 
     # call func to generate the tower
     tower = generate()
-    #for x in tower:
-    #    print('[%d, %d, %d]'%(x[0],x[1],x[2]))
-    #print(*tower, sep='\n')
-    #print('Slices: ', len(tower))
-
-    # tower with sol.
-    # tower=[deque([5,3,1]),deque([5,4,2]),deque([6,4,2]),deque([1,5,3]),deque([1,4,2]),deque([6,3,6])]
-    #tower with M.O.
-    tower=[deque([1,2,3]),deque([2,1,4]),deque([7,7,2]),deque([5,6,7]),deque([6,5,4])]
-    tower=[deque([3, 1, 2]), deque([1, 4, 2]), deque([6, 7, 5]), deque([5, 4, 6])]
-    print(tower)
     
-    sol= solve1 (tower)
-    # print('after solve\n\n\n')
+    # trys to solve original pizza slice tower
+    if not solve([deque(i) for i in tower]):
+        print('No solution.')
+    print('\n\n\nFinding Minimal Obsticle')
 
-    subsets = list(combinations(tower,4))
-    # print(subsets)
-    sub = []
-    for x in subsets:
-    #    print(type(x))
-       sub.append(list(x))
-       
-    # for x in sub:
-    #    print(x)
-    #    solve1(x)
-    print(sub[2])
-    s=solve1(sub[2])
+    # Creates combinations starting from 30 to 2 to find the minimal obsticle
+    for i in reversed(range(2,len(tower))):
+        found = False
+        #creats all combinations of size i
+        subsets = list(combinations(tower,i))
+        print(i)
+        for s in subsets:
+            #converts s from tuple to list
+            s = list(s)
+            #converts list to deque
+            t = [deque(x) for x in s]
+            # calls solve function on t (partial tower). if result is found function is done
+            if (solve(t)):
+                found = True
+                break
+            s.clear()
+        if found:
+            break
+        subsets.clear()
 
     end = datetime.datetime.now()
     print("Time =", end - start)
